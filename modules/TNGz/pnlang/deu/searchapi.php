8cd404d805a48177ff12bb707a723e2bfca5d90e
hilope <hilope@googlemail.com>
2008-10-14 10:38:20 +0000

    added german files part one (not completed yet)

---
+++ modules/TNGz/pnlang/deu/searchapi.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2002, PostNuke Development Team
+ * @link http://www.postnuke.com
+ * @version $Id$
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package PostNuke_Value_Addons
+ * @subpackage Pages
+ */
+
+define('_TNGZ_SEARCH_LIVING', 'Die folgenden Personen sind als lebend gekennzeichnet und wurden deshalb ausgeblendet.');
+define('_TNGZ_SEARCH', 'Ahnenforschungssuche');
+define('_TNGZ_SEARCH_BORN', 'geb.');
+define('_TNGZ_SEARCH_DIED', 'gest.');
+define('_TNGZ_SEARCH_RESULTS', 'Personen gefunden');
+define('_TNGZ_SEARCH_NONEFOUND', 'Niemand gefunden');
