4d4559def173a4271179811c62e76f05337e3aeb
hilope <hilope@googlemail.com>
2008-10-21 09:49:23 +0000

    added svn propset "Id" and modified file headers

--- pnlang/deu/admin.php
+++ pnlang/deu/admin.php
@@ -1,7 +1,19 @@
 <?php
-/************************************************************************/
-/* TNGz                    (pnlang/eng/admin.php)                      */
-/************************************************************************/
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2001, Zikula Development Team
+ * @link http://www.zikula.org
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * @package TNGz
+ * @url http://code.zikula.org/tngz
+ * @license http://www.gnu.org/copyleft/gpl.html
+ *
+ * @author Wendel Voigt
+ * @translator Sven Schomacker aka hilope
+ * @version $Id$
+ */
 
 /*---ADMIN-TEXT--------------------------------------------------------------*/
 define("_TNGZNAME"          , "TNGz");
