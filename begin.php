4e6c77e5e8520b70bb3df83f1fbaf6376021d47c
hilope <hilope@googlemail.com>
2008-10-21 09:48:44 +0000

    added svn propset "Id" and modified file headers

--- begin.php
+++ begin.php
@@ -1,2 +1,17 @@
 <?php
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2001, Zikula Development Team
+ * @link http://www.zikula.org
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * @package TNGz
+ * @url http://code.zikula.org/tngz
+ * @license http://www.gnu.org/copyleft/gpl.html
+ *
+ * @author Wendel Voigt
+ * @version $Id$
+ */
+
 ?>
--- pnversion.php
+++ pnversion.php
@@ -1,7 +1,19 @@
 <?php
-/************************************************************************/
-/* pnTNG                      (Version.php)                             */
-/************************************************************************/
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2001, Zikula Development Team
+ * @link http://www.zikula.org
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * @package TNGz
+ * @url http://code.zikula.org/tngz
+ * @license http://www.gnu.org/copyleft/gpl.html
+ *
+ * @author Wendel Voigt
+ * @version $Id$
+ */
+
 $modversion['name']           = 'TNGz';  //Module name
 $modversion['version']        = '1.01';  //Version number
 $modversion['description']    = 'TNG integration into Zikula';  //Module description
