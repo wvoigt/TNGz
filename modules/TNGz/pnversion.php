383bc7238de9ab9c680acf64c9c6301f73b4628c
hilope <hilope@googlemail.com>
2008-10-24 22:17:10 +0000

    removed deprecated api functions, code improvements, added dynamic language change for TNG

--- modules/TNGz/pnversion.php
+++ modules/TNGz/pnversion.php
@@ -21,7 +21,7 @@ $modversion['credits']        = 'pndocs/credits.txt';  //Credits file
 $modversion['help']           = 'pndocs/readme.html';  //Help file
 $modversion['changelog']      = 'pndocs/changelog.txt';  //Change log file
 $modversion['license']        = 'pndocs/license.txt';  //License file
-$modversion['official']       = '0';  //Official PostNuke Approved Module 1 = yes, 0 = no
+$modversion['official']       = '0';  //Official Zikula Approved Module 1 = yes, 0 = no
 $modversion['author']         = 'Wendel Voigt';  //Author
 $modversion['contact']        = 'http://code.zikula.org/tngz';  //The authors website or contact email address
 $modversion['admin']          = 1;
