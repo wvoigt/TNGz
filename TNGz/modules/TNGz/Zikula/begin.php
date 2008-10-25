c5873ecbfe324065314fbe59bb0c67840fb3aa9d
wvoigt <wvoigt@voigtworld.com>
2008-10-25 15:04:49 +0000

    Moved meta.php out of TNG directory and into TNGz directory so it is added automatically in installation.

--- TNGz/modules/TNGz/Zikula/begin.php
+++ TNGz/modules/TNGz/Zikula/begin.php
@@ -1,4 +1,19 @@
 <?php
-  // Dummy begin.php file for TNG.
-  // This file *must* be in the Zikula root directory or TNG will give errors.
-
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2001, Zikula Development Team
+ * @link http://www.zikula.org
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * @package TNGz
+ * @url http://code.zikula.org/tngz
+ * @license http://www.gnu.org/copyleft/gpl.html
+ *
+ * @author Wendel Voigt
+ * @version $Id: pnadminapi.php 29 2008-10-21 10:01:35Z hilope $
+ *
+ * This file is required for proper opperation of TNG with the TNGz module.
+ * TNG always includes a file called "begin.php" from the default include directory.
+ * When used in Zikula from the TNGz module, TNG expects to find this file in the base Zikula directory.
+ */
