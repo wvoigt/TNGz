d56806191d4e11ce3b51f01ca8e9cef0accd8aad
hilope <hilope@googlemail.com>
2008-10-30 18:21:31 +0000

    simplified german lang files

--- TNGz/modules/TNGz/pnlang/deu/MostWanted.php
+++ TNGz/modules/TNGz/pnlang/deu/MostWanted.php
@@ -15,29 +15,4 @@
  * @version $Id$
  */
 
-define('_ACCESSTNG',      'Ahnenforschungsseite');
-define('_NONETNG',        'Niemand für heute gefunden');
-define('_PEOPLEDBFERROR', 'Fehler im Zugriff auf TNG-Tabelle. ');
-define('_SELECTYES',      'Ja');
-define('_SELECTNO',       'Nein');
-define('_SHOWORDER',      'Zeige Einträge nach :');
-define('_SELECTORDERN',   'Nachname');
-define('_SELECTORDERD',   'Neuestem zu Frühestem');
-define('_SELECTORDERR',   'Frühestem zu Neuestem');
-define('_SELECTORDERE',   'Eingegebene Reihenfolge');
-
-define('_SHOWFAMILYNAME',     'Zeige Familien nach :');
-define('_SELECTFAMILYFULL',   'Voller Name');
-define('_SELECTFAMILYSHORT',  'nur Nachnamen');
-
-define('_WANTEDINSTRUCTIONS', '<strong>Unten stehende Einträge sind optional.</strong><br />Leere Einträge werden nicht in die Ausgabe übergeben.<br />IDs, welche nicht im richtigen Format sind, nicht in TNG vorhanden sind oder Fehler beinhalten werden in die Ausgabe übernommen.');
-define('_WANTEDTEXT',         'Beschreibung im oberen Teil des Blocks (kann HTML enthalten)');
-define('_WANTEDPEOPLELABEL',  'Titel der Personenliste (z.B. Personen)');
-define('_WANTEDPEOPLELIST',   'Liste der PersonIDs (z.B. I001, I002)');
-define('_WANTEDFAMILYLABEL',  'Titel der Familienliste (z.B. Families)');
-define('_WANTEDFAMILYLIST',   'Liste der FamilyIDs (z.B. F001, F002)');
-define('_WANTEDMENULINK',     'Zeige TNG-Menülink unten?');
-define('_MARRIAGE_AND',   'und');
-define('_MARRIED_ABR',     'm.');
-define('_DIVORCED_ABR',    'D');
-
+include_once('modules/TNGz/pnlang/deu/global.php');
--- TNGz/modules/TNGz/pnlang/deu/RandomPhoto.php
+++ TNGz/modules/TNGz/pnlang/deu/RandomPhoto.php
@@ -15,22 +15,4 @@
  * @version $Id$
  */
 
-define('_SHOWLIVING',     'Zeige Fotos der Personen');
-define('_SELECTLIVINGY',  'Ja, zeige alle');
-define('_SELECTLIVINGN',  'Nein');
-define('_SELECTLIVINGD',  'Ja, aber keine lebenden Personen');
-define('_SELECTLIVINGL',  'Ja, aber zeig lebende Personen nur eingeloggten Benutzern');
-//
-define('_SHOWPHOTO',      'Welcher Typ von Foto soll benutzt werden');
-define('_SELECTPHOTOT',   'Thumbnail de Fotos');
-define('_SELECTPHOTOP',   'Aktuelles Foto');
-//
-define('_MAXHEIGHT',      'Maximale Höhe der Fotos');
-define('_MAXWIDTH',       'Maximale Breite der Fotos');
-//
-define('_TNGPHOTO',       'Fotograf');
-define('_PEOPLEDBFERROR', 'Fehler im Zugriff auf TNG-Tabelle.');
-define('_NOPHOTOFOUND',   'Kein Foto gefunden');
-define('_USECACHE',       'Blcok-Caching aktivieren (wenn pnRender-Caching aktiviert ist)');
-define('_TNGPHOTOLIST',   'Limit Photo selection to the following list of TNG mediaID numbers (default = blank)');
-
+include_once('modules/TNGz/pnlang/deu/global.php');
