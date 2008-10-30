6112b4061ba1e08b46e115e0b77c1c7f22f72473
hilope <hilope@googlemail.com>
2008-10-30 18:21:31 +0000

    simplified german lang files

--- modules/TNGz/pnlang/deu/ThisDay.php
+++ modules/TNGz/pnlang/deu/ThisDay.php
@@ -15,35 +15,4 @@
  * @version $Id$
  */
 
-define('_SHOWDATE',       'Zeige heutiges Datum :');
-define('_SHOWBIRTH',      'Zeige Geburtstage :');
-define('_SHOWMARRIAGE',   'Zeige Hochzeitstage :');
-define('_SHOWDEATH',      'Zeige Todestage :');
-define('_SHOWORDER',      'Sortiere Einträge nach :');
-define('_SHOWWIKI',       'Zeige Wikipedia-Link für aktuellen Tag :');
-define('_SELECTBIRTHY',   'Ja, zeige alles');
-define('_SELECTBIRTHN',   'Nein');
-define('_SELECTBIRTHD',   'Ja, aber keine lebenden Personen');
-define('_SELECTBIRTHL',   'Ja, aber lebende Personen nur eingeloggten Benutzern zeigen');
-define('_SELECTMARRIAGEY','Ja, zeige alle');
-define('_SELECTMARRIAGEN','Nein');
-define('_SELECTMARRIAGED','Ja, aber keine lebenden Personen');
-define('_SELECTMARRIAGEL','Ja, aber lebende Personen nur eingeloggten Benutzern zeigen');
-define('_SELECTDEATHY',   'Ja');
-define('_SELECTDEATHN',   'Nein');
-define('_SELECTORDERN',   'Nachname, Vorname');
-define('_SELECTORDERD',   'Datum des Eintrages - ältester zu neuestem');
-define('_SELECTORDERR',   'Datum des Eintrages - neuester zu ältestem');
-define('_SELECTYES',      'Ja');
-define('_SELECTNO',       'Nein');
-define('_PEOPLEDBFERROR', 'Fehler im Zugriff auf TNG-Tabelle. ');
-define('_BIRTH',          'Geburtstage');
-define('_MARRIAGE',       'Hochzeitstage');
-define('_MARRIAGE_AND',   'und');
-define('_DIVORCED',       'D');
-define('_DEATH',          'Todestage');
-define('_ACCESSTNG',      'Ahnenforschungsseite');
-define('_NONETNG',        'Niemand für heute gefunden');
-define('_WIKITODAY',      'Gehe zu Wikipedia');
-define('_USECACHE',       'Block-Caching aktivieren (wenn pnRender-Caching aktiviert ist)');
-
+include_once('modules/TNGz/pnlang/deu/global.php');
