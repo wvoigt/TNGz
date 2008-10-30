fdecf32a97ad094ce4ecacd455314056a1864e65
hilope <hilope@googlemail.com>
2008-10-30 18:21:31 +0000

    simplified german lang files

--- pnlang/deu/WhatsNew.php
+++ pnlang/deu/WhatsNew.php
@@ -15,23 +15,4 @@
  * @version $Id$
  */
 
-define('_TNGZ_WHATSNEW_RECENT',      'neu seit ');
-define('_TNGZ_WHATSNEW_DAYS',        'Tagen');
-define('_TNGZ_WHATSNEW_HEAD_PEOPLE', 'Personen');
-define('_TNGZ_WHATSNEW_PEOPLE',      'Zeige neue oder geänderte Personen');
-define('_TNGZ_WHATSNEW_HEAD_FAMILY', 'Familien');
-define('_TNGZ_WHATSNEW_FAMILY',      'Zeige neue oder geänderte Familien');
-define('_TNGZ_WHATSNEW_HEAD_PHOTOS', 'Fotos');
-define('_TNGZ_WHATSNEW_PHOTOS',      'Zeige neue oder geänderte Fotos');
-define('_TNGZ_WHATSNEW_HEAD_HISTORY','Historien');
-define('_TNGZ_WHATSNEW_HISTORY',     'Zeige neue oder geänderte Historien');
-define('_TNGZ_WHATSNEW_HOWMANY',     'Maximale Anzahl der Einträge in Darstellung');
-define('_TNGZ_WHATSNEW_HOWMANY_NUM', '10');
-define('_TNGZ_WHATSNEW_HOWLONG',     'Maximale Anzahl der Tage in Darstellung');
-define('_TNGZ_WHATSNEW_HOWLONG_NUM', '30');
-define('_TNGZ_WHATSNEW_YES',         'Ja');
-define('_TNGZ_WHATSNEW_NO',          'Nein');
-define('_TNGZ_WHATSNEW_NOCHANGES',   'Keine neuen oder geänderten Einträge');
-define('_TNGZ_WHATSNEW_SQLERROR',    'Fehler im Datenbankzugriff.');
-define('_USECACHE',                  'Block-Caching aktivieren (wenn pnRender-Caching aktiviert ist)');
-
+include_once('modules/TNGz/pnlang/deu/global.php');
