29cf788c0db761ac65dd536e3cc98c665928669e
hilope <hilope@googlemail.com>
2008-11-28 14:28:49 +0000

    new defines for surnames cloud and map views

---
+++ modules/TNGz/pnlang/deu/user.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2001, Zikula Development Team
+ * @link http://www.zikula.org
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * @package TNGz
+ * @url http://code.zikula.org/tngz
+ * @license http://www.gnu.org/copyleft/gpl.html
+ *
+ * @author Wendel Voigt
+ * @translator Sven Schomacker
+ * @version $Id$
+ */
+
+// common defines
+define("_TNGZ_SHOWTOP", "Zeige Top ");
+define("_TNGZ_BYOCCURRENCE", " nach Häufigkeit sortiert");
+define("_TNGZ_GO", "Zeige");
+define("_TNGZ_SORTEDALPHA", " alphabetisch sortiert");
+
+// places item
+define("_TNGZ_PLACETITLE", "Top Orte");
+define("_TNGZ_TOPPLACES", "Top Orte");
+define("_TNGZ_TOTALPLACES", "Orte gesamt");
+define("_TNGZ_SHOWALLPLACES", "Zeige alle Orte");
+define("_TNGZ_MAINPLACESPAGE", "Hauptansicht der Orte");
+
+// surnames item
+define("_TNGZ_SURNAMETITLE", "Top Nachnamen");
+define("_TNGZ_TOP", "Top");
+define("_TNGZ_SURNAMES", "Nachnamen");
+define("_TNGZ_OUTOF", "von");
+define("_TNGZ_TOTALSURNAMES", "eindeutigen Nachnamen gesamt");
+define("_TNGZ_SHOWALLSURNAMES", "Alle Nachnamen anzeigen");
+define("_TNGZ_MAINSURNAMESPAGE", "Hauptansicht der Nachnamen");
+
