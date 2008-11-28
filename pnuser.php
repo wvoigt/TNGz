be1a653df7f3950f2d8828d5039c5864eb5024dd
hilope <hilope@googlemail.com>
2008-11-28 13:55:19 +0000

    deleted wrong url encoding and added utf8 encoding for sitemap

--- pnuser.php
+++ pnuser.php
@@ -13,15 +13,15 @@
  * @author Wendel Voigt
  * @version $Id$
  */
- 
+
 /**
 * TNG interface in Zikula
-* 
+*
 * Displays the given TNG page either wrapped in Zikula or not, depending upon the page given
-* 
-* @param show TNG page to display.  Additional arguments may be used by TNG 
+*
+* @param show TNG page to display.  Additional arguments may be used by TNG
 * @return true if the page has already been displayed, othersise the TNG data to be used in Zikula
-*/ 
+*/
 function TNGz_user_main() {
 
     $TNGpage = FormUtil::getPassedValue('show', 'index', 'GET');
@@ -54,10 +54,10 @@ function TNGz_user_main() {
 
 /**
 * TNG administration in Zikula
-* 
+*
 * Checks for logged in Zikula user and permissions, then sets up TNG administration in an IFRAME.
-* 
-* @param none 
+*
+* @param none
 * @return IFRAME page calling TNG administration
 */
 function TNGz_user_admin() {
@@ -91,12 +91,12 @@ function TNGz_user_admin() {
 
 /**
 * TNG sitemap generation
-* 
+*
 * Produce three different types of sitemaps
 * 1. If no parameters are used, a full sitemap if the number of entries is below a set threshold
 * 2. If no parameters are used, a sitemapindex file if the number of entries is above a set threshold
 * 3. If parameters are used, a partial sitemap file that can be called by the sitemapindex file.
-* 
+*
 * @param map    (optional) subset record type on which to generate a sitemap (e.g., people, family, etc.)
 * @param start  (optional) the first record number to show
 * @param count  (optional) the number of records to show
@@ -105,14 +105,14 @@ function TNGz_user_admin() {
 function TNGz_user_sitemap() {
     if (!pnSecAuthAction(0, 'TNGz::', '::', ACCESS_READ)) {
         return pnVarPrepHTMLDisplay(_MODULENOAUTH);
-    }   
+    }
 
     $none = "-1";  // flag
-   
+
     $map   = FormUtil::getPassedValue('map',   $none, 'GET');
     $start = FormUtil::getPassedValue('start', $none, 'GET');
     $count = FormUtil::getPassedValue('count', $none, 'GET');
-    
+
     $records = array();
 
     switch ($map) {
@@ -145,10 +145,10 @@ function TNGz_user_sitemap() {
 
     // Return a sitemap (either full, or a partial one)
     $pnRender =& new pnRender('TNGz');
-    $pnRender->assign('records', $records);
+    $pnRender->assign('records', utf8_encode($records));
     $pnRender->display('TNGz_user_sitemap.htm');
     return true;
-    
+
 }
 
 function TNGz_user_view()
@@ -160,8 +160,8 @@ function TNGz_user_view()
     $item   = FormUtil::getPassedValue('item', false, 'GET');
     $validitems = array('places', 'surnames');
     $item = (in_array($item, $validitems))? $item : $validitems[0];
-    
-    
+
+
     $TNG = pnModAPIFunc('TNGz','user','GetTNGpaths');
 
     // Check to be sure we can get to the TNG information
@@ -175,12 +175,12 @@ function TNGz_user_view()
     if (!$have_info) {
         return pnVarPrepHTMLDisplay("Failed to find TNG database");
     }
-    
-    $cms['tngpath']    = $TNG['directory']. "/";        
+
+    $cms['tngpath']    = $TNG['directory']. "/";
     if ($item == "places" ) {
 
         $thePlaces = array();
-        
+
         $query = "SELECT distinct trim(substring_index(place,',',-1)) as myplace, count(distinct place) as placecount FROM $places_table WHERE trim(substring_index(place,',',-1)) != \"\" GROUP BY myplace ORDER by placecount DESC LIMIT 30";
         $saved_fetch_mode = &$TNG_conn->SetFetchMode(ADODB_FETCH_ASSOC);
         if (!$result = &$TNG_conn->Execute($query) ) {
@@ -206,7 +206,7 @@ function TNGz_user_view()
         }
         $saved_fetch_mode= &$TNG_conn->SetFetchMode($saved_fetch_mode);
         $TNG_conn->Close();
-        
+
         // Need to clean this up and put in lang files
         $text['placetitle']    = "Top Locations and Places";
         $text['topplaces']     = "Top Places";
@@ -216,8 +216,8 @@ function TNGz_user_view()
         $text['sortedalpha']   = "sorted alphabetically";
         $text['mainplacepage'] = "Main place listing page";
         $text['go']            = "Go";
-        
-        
+
+
         $pnTNGmodinfo = pnModGetInfo(pnModGetIDFromName('TNGz'));
 
         $pnRender =& new pnRender('TNGz');
@@ -226,7 +226,7 @@ function TNGz_user_view()
         $pnRender->assign('text'     , $text);
         return $pnRender->fetch('TNGz_user_view_places.htm');
     }
-    
+
     if ($item == "surnames" ) {
 
         $default_top = 100;
@@ -246,8 +246,8 @@ function TNGz_user_view()
         $SurnameRank  = array();
         for ($rank=1; !$result->EOF && $rank<=$top; $result->MoveNext(), $rank++) {
             $name = $result->fields;
-            $name['surname']   = urlencode($name['surname']);
-            $name['surnameuc'] = urlencode($name['surnameuc']);            
+            $name['surname']   = $name['surname'];
+            $name['surnameuc'] = urlencode($name['surnameuc']);
             $name['rank']      = $rank;
             // Now assign a class to each surname based upon relative number to most used surname
             $percent = 100 * $name['count'] / $SurnameMax;
@@ -273,10 +273,10 @@ function TNGz_user_view()
                 $class = 0;
             }
             $name['class'] = $class;
-            
+
             $SurnameRank[]  = $name;
         }
-        
+
         // Now get a array $SurnameAlpha sorted on the upper case surname
         $SurnameAlpha = $SurnameRank;
         foreach ($SurnameAlpha as $key => $row) {
@@ -319,12 +319,12 @@ function TNGz_user_worldmap()
 {
     if (!pnSecAuthAction(0, 'TNGz::', '::', ACCESS_READ)) {
         return pnVarPrepHTMLDisplay(_MODULENOAUTH);
-    }   
- 
+    }
+
     $size    = FormUtil::getPassedValue('size'  , false, 'GET');
     $region  = FormUtil::getPassedValue('region', 0,     'GET');
     $region = (in_array($region, array(0,1,2,3,4,5,6)))? $region : 0;
-      
+
     $TNG = pnModAPIFunc('TNGz','user','GetTNGpaths');
 
     // Check to be sure we can get to the TNG information
@@ -346,7 +346,7 @@ function TNGz_user_worldmap()
     if (!$result = &$TNG_conn->Execute($query) ) {
         return(false);
     }
-    $points = array();    
+    $points = array();
     if($result->RecordCount()>0) {
         for (; !$result->EOF; $result->MoveNext()) {
             $items = array();
@@ -355,12 +355,12 @@ function TNGz_user_worldmap()
         }
     }
     mysql_free_result($result);
-    $TNG_conn->Close();    
-         
+    $TNG_conn->Close();
+
     switch ($size) {
         case 'tiny':
                   $ratio = 0.25;
-	              break;        
+	              break;
         case 'small':
                   $ratio = 0.50;
 	              break;
@@ -372,12 +372,12 @@ function TNGz_user_worldmap()
 		          break;
         case 'giant':
 	              $ratio = 1.50;
-		          break; 
-        case 'large':                
+		          break;
+        case 'large':
 	    default:
 	              $ratio = 1.0;
 		          break;
-    }    
+    }
 
     // Load the background map
     $imagefile = "800px-Whole_world_-_land_and_oceans.jpg";
@@ -389,33 +389,33 @@ function TNGz_user_worldmap()
     $source_y = imagesy($source);
 
     // Create an image based upon the source (same size)
-    $im = imagecreatetruecolor($source_x, $source_y);        
+    $im = imagecreatetruecolor($source_x, $source_y);
     imagecopyresized($im, $source, 0, 0, 0, 0, $source_x , $source_y, $source_x, $source_y);
     imagedestroy($source);
-    
+
     // find the base image size
     $scale_x = imagesx($im);
-    $scale_y = imagesy($im);    
-        
+    $scale_y = imagesy($im);
+
     $red = imagecolorallocate ($im, 255,0,0);
 
    // Now we convert the long/lat coordinates into image coordinates
    foreach($points as $point){
        $pt = TNGz_map_coordinates($point['latitude'], $point['longitude'], $scale_x, $scale_y);
-       imagefilledellipse($im,$pt["x"],$pt["y"],2,2,$red); // mark on the map   
+       imagefilledellipse($im,$pt["x"],$pt["y"],2,2,$red); // mark on the map
    }
-   
+
    // Define the partial regions.  Region 0 is everything.
    // Regions are arranged as:  1  2  3 <= Northern Hemisphere
    //                           4  5  6 <= Southern Hemisphere
-   $regions[0]=array('y'=> 0.00, 'h'=> 1.00, 'x'=> 0.00,                              'w'=> 1.00 );   
+   $regions[0]=array('y'=> 0.00, 'h'=> 1.00, 'x'=> 0.00,                              'w'=> 1.00 );
    $regions[1]=array('y'=> 0.00, 'h'=> 0.50, 'x'=> 0.00,                              'w'=> 0.45 );
    $regions[2]=array('y'=> 0.00, 'h'=> 0.50, 'x'=> $regions[1]['w'],                  'w'=> 0.30 );
    $regions[3]=array('y'=> 0.00, 'h'=> 0.50, 'x'=> $regions[1]['w']+$regions[2]['w'], 'w'=> 1- $regions[1]['w']-$regions[2]['w']);
    $regions[4]=array('y'=> 0.50, 'h'=> 0.50, 'x'=> $regions[1]['x'],                  'w'=> $regions[1]['w'] );
    $regions[5]=array('y'=> 0.50, 'h'=> 0.50, 'x'=> $regions[2]['x'],                  'w'=> $regions[2]['w'] );
    $regions[6]=array('y'=> 0.50, 'h'=> 0.50, 'x'=> $regions[3]['x'],                  'w'=> $regions[3]['w'] );
-  
+
    $part_x = $source_x * $regions[$region]['x'];
    $part_w = $source_x * $regions[$region]['w'];
    $part_y = $source_y * $regions[$region]['y'];
@@ -430,12 +430,12 @@ function TNGz_user_worldmap()
    // Now create the final image, then copy (all or part) and resize
    $im_return = imagecreatetruecolor($dest_w, $dest_h);
    imagecopyresized($im_return, $im, $dest_x, $dest_y, $part_x, $part_y, $dest_w, $dest_h, $part_w, $part_h);
-   imagedestroy($im); 
+   imagedestroy($im);
 
    // Return the map image. Uing a PNG format since it gives better final image quality
    header ("Content-type: image/png");
    imagepng($im_return);
-   imagedestroy($im_return); 
+   imagedestroy($im_return);
    return true;
 }
 
