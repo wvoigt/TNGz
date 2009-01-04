9358efc2c0d616ef491c1fa0f302099961620d66
wvoigt <wvoigt@voigtworld.com>
2009-01-04 17:59:26 +0000

    For lang eng, moved all items to common.php (which is loaded automatically when the module is loaded.)  Added require_once in all the other files, just to be sure it is loaded.  Now all eng language constants are together in one file.

--- TNGz/modules/TNGz/pnlang/eng/WhatsNew.php
+++ TNGz/modules/TNGz/pnlang/eng/WhatsNew.php
@@ -14,23 +14,5 @@
  * @version $Id$
  */
 
-define('_TNGZ_WHATSNEW_RECENT',      'newest in');
-define('_TNGZ_WHATSNEW_DAYS',        'days');
-define('_TNGZ_WHATSNEW_HEAD_PEOPLE', 'Individuals');
-define('_TNGZ_WHATSNEW_PEOPLE',      'Show new or changed Individuals');
-define('_TNGZ_WHATSNEW_HEAD_FAMILY', 'Families');
-define('_TNGZ_WHATSNEW_FAMILY',      'Show new or changed Families');
-define('_TNGZ_WHATSNEW_HEAD_PHOTOS', 'Photos');
-define('_TNGZ_WHATSNEW_PHOTOS',      'Show new or changed Photos');
-define('_TNGZ_WHATSNEW_HEAD_HISTORY','Histories');
-define('_TNGZ_WHATSNEW_HISTORY',     'Show new or changed Histories');
-define('_TNGZ_WHATSNEW_HOWMANY',     'Maximum number of entries to display');
-define('_TNGZ_WHATSNEW_HOWMANY_NUM', '10');
-define('_TNGZ_WHATSNEW_HOWLONG',     'Maximum number of days to to display');
-define('_TNGZ_WHATSNEW_HOWLONG_NUM', '30');
-define('_TNGZ_WHATSNEW_YES',         'Yes');
-define('_TNGZ_WHATSNEW_NO',          'No');
-define('_TNGZ_WHATSNEW_NOCHANGES',   'No new or updated records');
-define('_TNGZ_WHATSNEW_SQLERROR',    'Error in accessing the database.');
-define('_USECACHE',                  'Cache the block (if pnRender caching is enabled)');
+require_once dirname(realpath(__FILE__)).'/common.php';
 
