301ac53e7628b6c878aabf61db3ef84e3d328706
hilope <hilope@googlemail.com>
2009-02-17 06:32:29 +0000

    added needed files for Zikula installations

---
+++ begin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * @copyright  (c) Zikula Development Team
+ * @link       http://www.zikula.org
+ * @version    $Id$
+ * @license    GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @author     Wendel Voigt
+ * @category   Zikula_Extension
+ * @package    Content
+ * @subpackage TNGz
+ *
+ *
+ * This file is required for proper opperation of TNG with the TNGz module.
+ * TNG always includes a file called "begin.php" from the default include directory.
+ * When used in Zikula from the TNGz module, TNG expects to find this file in the base Zikula directory.
+ */
